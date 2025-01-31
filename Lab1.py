b582f705b663577b4ff8095479eae4aec64822c5
Chris-Yan567 <moej70899@gmail.com>
2025-01-31 13:20:42 +0000

    pytest works now but first question pass and second fails

--- Lab1.py
+++ Lab1.py
@@ -1,15 +1,15 @@
 # Function 1: Write a simple Hello World program
 # This function should print "Hello, World!" to the screen.
 def hello_world():
- print("Hello, World!")
+    print("Hello, World!")
 
 # Function 2: Get input and output with different variable types
 # This function should prompt the user for their name (string), age (int), and height (float),
 # and then print them back in a formatted message.
 def input_output():
-name= input("Whats youre name? ")
-age = int(input("how old are you? "))
-height= float(input("How tall are you in cm? "))
-print("your name is ", name)
-print("you are ", age, "years old")
-print("you are ", height, "cm tall")
+    name= input("Whats your name? ")
+    age = int(input("how old are you? "))
+    height= float(input("How tall are you in cm? "))
+    print("your name is ", name)
+    print("you are ", age, "years old")
+    print("you are ", height, "cm tall")
